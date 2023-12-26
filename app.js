f160dc818dea09563359465e9570d581f89234b7
Mritunjay <mupadhyay00@gmail.com>
2023-12-26 20:03:50 +0700

    📝 chore: add ESLint and Prettier configuration files
    📝 chore: add .prettierignore file to specify files to be ignored by Prettier
    
    📝 chore: add .prettierrc.json file to configure Prettier formatting options
    
    📝 chore: update README.md
    
    📝 refactor(app.js): improve code formatting and organization
    
    📝 refactor(index.js): improve code formatting and organization
    
    📝 chore(package.json): add scripts for code formatting and linting

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-import express from "express";
-import cors from "cors";
-import 'dotenv/config';
-import { createServer } from "http";
-const app = express();
-const httpServer = createServer(app);
-
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import { createServer } from 'http'
+const app = express()
+const httpServer = createServer(app)
 
 const corsOptions = {
     origin: [process.env.CORS_ORIGIN, process.env.CORS_ORIGIN2],
     credentials: true,
-  };
+}
 // global middlewares
-app.use(cors(corsOptions));
+app.use(cors(corsOptions))
 
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.json({ limit: '16kb' }))
+app.use(express.urlencoded({ extended: true, limit: '16kb' }))
 
-export { httpServer };
+export { httpServer }
--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-import { httpServer } from "./app.js";
+import { httpServer } from './app.js'
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000
 const startServer = () => {
-    
     httpServer.listen(PORT, () => {
-      console.log("⚙️  Server is running on port: " + PORT);
-    });
-};
+        console.log('⚙️  Server is running on port: ' + PORT)
+    })
+}
 
 try {
-    startServer();
-  } catch (err) {
-    console.log("connect error: ", err);
+    startServer()
+} catch (err) {
+    console.log('connect error: ', err)
 }
